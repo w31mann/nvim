50985ff0d37486709f9964bf0e5c8d94ee90866b
user <user@eax.ninja>
2024-12-21 15:13:41 +0100

    refactor(blink): fine tune pattern matching

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,6 +4,7 @@ return {
     "saghen/blink.cmp",
     -- version = "v0.*",
     build = "cargo build --release --locked",
+    event = { "CmdlineEnter", "InsertEnter" },
     dependencies = {
         {
             -- luasnip - https://github.com/L3MON4D3/LuaSnip
@@ -24,6 +25,7 @@ return {
             ["<c-z>"] = { "accept" },
         },
         completion = {
+            keyword = { range = "full" },
             menu = {
                 border = vim.g.BORDER,
             },
