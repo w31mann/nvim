c2e64c5d4b5a82372dbde6d73ca0f3b143d10b51
user <user@eax.ninja>
2025-02-02 17:40:59 +0100

    feat(mini.ai): add mini.ai

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -48,7 +48,7 @@ return {
                 },
             },
             ghost_text = {
-                enabled = true,
+                -- enabled = true,
             },
         },
         signature = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
         -- treesitter-context - https://github.com/nvim-treesitter/nvim-treesitter-context
         "nvim-treesitter/nvim-treesitter-context",
         -- treesitter-textobjects - https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        -- "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
         ---@diagnostic disable: missing-fields
@@ -65,46 +65,50 @@ return {
             indent = {
                 enable = true,
             },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<c-space>",
-                    node_incremental = "<c-space>",
-                    scope_incremental = "<c-s>",
-                    node_decremental = "<c-s-space>",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ab"] = "@block.outer",
-                        ["ib"] = "@block.inner",
-                        ["ac"] = "@conditional.outer",
-                        ["ic"] = "@conditional.inner",
-                        ["al"] = "@loop.outer",
-                        ["il"] = "@loop.inner",
-                        ["as"] = "@scope.inner",
-                        ["is"] = "@scope.inner",
-                        ["aC"] = "@comment.outer",
-                        ["iC"] = "@comment.inner",
-                    },
-                },
-                lsp_interop = {
-                    enable = true,
-                    border = vim.g.BORDER,
-                    floating_preview_opts = {},
-                    peek_definition_code = {
-                        ["<leader>df"] = "@function.outer",
-                        ["<leader>dF"] = "@class.outer",
-                    },
-                },
-            },
+            -- incremental_selection = {
+            --     enable = true,
+            --     keymaps = {
+            --         -- init_selection = "<c-space>",
+            --         -- node_incremental = "<c-space>",
+            --         -- scope_incremental = "<c-s>",
+            --         -- node_decremental = "<c-s-d>",
+            --         init_selection = "gnn",
+            --         node_incremental = "gin",
+            --         scope_incremental = "gis",
+            --         node_decremental = "gdn",
+            --     },
+            -- },
+            -- textobjects = {
+            --     select = {
+            --         enable = true,
+            --         -- Automatically jump forward to textobj, similar to targets.vim
+            --         lookahead = true,
+            --         keymaps = {
+            --             -- You can use the capture groups defined in textobjects.scm
+            --             ["af"] = "@function.outer",
+            --             ["if"] = "@function.inner",
+            --             ["ab"] = "@block.outer",
+            --             ["ib"] = "@block.inner",
+            --             ["ac"] = "@conditional.outer",
+            --             ["ic"] = "@conditional.inner",
+            --             ["al"] = "@loop.outer",
+            --             ["il"] = "@loop.inner",
+            --             ["as"] = "@scope.outer",
+            --             ["is"] = "@scope.inner",
+            --             ["aa"] = "@assignment.lhs",
+            --             ["ia"] = "@assignment.rhs",
+            --         },
+            --     },
+            --     -- lsp_interop = {
+            --     --     enable = true,
+            --     --     border = vim.g.BORDER,
+            --     --     floating_preview_opts = {},
+            --     --     peek_definition_code = {
+            --     --         ["<leader>df"] = "@function.outer",
+            --     --         ["<leader>dF"] = "@class.outer",
+            --     --     },
+            --     -- },
+            -- },
         })
     end,
 }
