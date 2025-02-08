05dc57613fdd01df733fcf8eaa29b275052b727f
user <user@eax.ninja>
2025-02-08 11:54:53 +0100

    feat(mini-ai): update settings

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,23 +1,22 @@
 return {
     "echasnovski/mini.ai",
     version = false,
-    enabled = true,
     dependencies = {
         -- treesitter-context - https://github.com/nvim-treesitter/nvim-treesitter-context
         "nvim-treesitter/nvim-treesitter-context",
         -- treesitter-textobjects - https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        -- "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
         local mini_ai = require("mini.ai")
-        local spec_treesitter = mini_ai.gen_spec.treesitter
+        -- local spec_treesitter = mini_ai.gen_spec.treesitter
 
         mini_ai.setup({
             n_lines = 200,
-            custom_textobjects = {
-                S = spec_treesitter({ a = "@scope.outer", i = "@scope.inner"}),
-                F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
-            },
+            -- custom_textobjects = {
+            --     S = spec_treesitter({ a = "@scope.outer", i = "@scope.inner"}),
+            --     F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+            -- },
         })
     end,
 }
