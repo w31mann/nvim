9cc0c856aab6bfee21f5c24f2a299bb2a5a7fbaa
user <user@eax.ninja>
2025-04-15 20:21:25 +0200

    refactor(jsonls): remove schemastore

--- lua/plugins/lsp/settings/jsonls.lua
+++ lua/plugins/lsp/settings/jsonls.lua
@@ -5,46 +5,38 @@ return {
     settings = {
         json = {
             validate = { enable = true },
-            schemas = require("schemastore").json.schemas({
+            schemas = {
                 extra = {
                     {
-                        name = "ucm_swpkg",
                         description = "UCM SW package",
-                        fileMatch = { "manifest.json" },
+                        fileMatch = { "swpkg/manifest.json" },
                         -- folderUri =
                         -- "/home/user/src/mwd_internal/over-the-air/ucm/examples/configs/pkg-manifests/swpkg",
-                        url =
-                        "file://" .. root_dir .. "/schemas/swpkg/manifest.schema.json",
+                        url = "file://" .. root_dir .. "/schemas/swpkg/manifest.schema.json",
                     },
                     {
-                        name = "ucm_swcl",
                         description = "UCM SW cluster",
-                        fileMatch = { "manifest.json" },
+                        fileMatch = { "swcl/manifest.json" },
                         -- folderUri =
                         -- "/home/user/src/mwd_internal/over-the-air/ucm/examples/configs/pkg-manifests/swcl",
-                        url =
-                        "file://" .. root_dir .. "/schemas/swcl/manifest.schema.json",
+                        url = "file://" .. root_dir .. "/schemas/swcl/manifest.schema.json",
                     },
                     {
-                        name = "ucm_cfg",
                         description = "UCM config",
                         fileMatch = { "ucm.json" },
                         -- folderUri =
                         -- "/home/user/src/mwd_internal/over-the-air/ucm/examples/configs/ucm-daemon",
-                        url =
-                        "file://" .. root_dir .. "/schemas/ucm-daemon/ucm.schema.json",
+                        url = "file://" .. root_dir .. "/schemas/ucm-daemon/ucm.schema.json",
                     },
                     {
-                        name = "fd-reader_cfg",
                         description = "fs-reader config",
                         fileMatch = { "fs-reader.json" },
                         -- folderUri =
                         -- "/home/user/src/mwd_internal/over-the-air/ucm/update-verification/fs-reader/examples/configs",
-                        url =
-                        "file://" .. root_dir .. "/schemas/fs-reader.schema.json",
+                        url = "file://" .. root_dir .. "/schemas/fs-reader.schema.json",
                     },
                 },
-            }),
+            },
         },
     },
 }
--- lua/plugins/schemastore.lua
+++
@@ -1,5 +0,0 @@
--- schemastore -- https://github.com/b0o/SchemaStore.nvim
-
-return {
-    "b0o/schemastore.nvim",
-}
