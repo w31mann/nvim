7a92e1e7fe6937481de4830a0138033b9040bf12
Philip Weimann <philip.weimann@aptiv.com>
2025-05-28 15:19:53 +0200

    refactor: replace illuminate with nvim native config

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,7 +1,6 @@
 local core_augroup = vim.api.nvim_create_augroup("core_augroup", {
     clear = true,
 })
-
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
@@ -18,4 +17,27 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 vim.api.nvim_create_autocmd("VimResized", {
     command = "wincmd =",
     group = core_augroup,
+})
+
+-- highlight word under cursor using LSP
+local cursorword_augroup = vim.api.nvim_create_augroup("cursorword_augroup", {
+    clear = true,
+})
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function(event_args)
+        local line_count = vim.api.nvim_buf_line_count(event_args.buf)
+        if line_count < 10000 then
+            vim.lsp.buf.document_highlight()
+        end
+    end,
+    group = cursorword_augroup,
+})
+vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter" }, {
+    callback = function(event_args)
+        local line_count = vim.api.nvim_buf_line_count(event_args.buf)
+        if line_count < 10000 then
+            vim.lsp.buf.clear_references()
+        end
+    end,
+    group = cursorword_augroup,
 })
