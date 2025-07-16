522d3be91e8d77525c471561a757710889add98f
Philip Weimann <philip.weimann@aptiv.com>
2025-07-16 12:39:01 +0200

    feat(markdown): use a different markdown rendering plugin

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,26 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = true,
+    ft = "markdown",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("render-markdown").setup({
+            anti_conceal = {
+                enabled = false,
+                ignore = {
+                    sign = true,
+                    code_language = true,
+                    code_background = true,
+                    code_border = true,
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>tm", ":RenderMarkdown toggle<cr>", {
+            desc = "Toggle markdown concealing",
+        })
+    end,
+}
