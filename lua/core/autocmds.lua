f330857fb387d4168c8207059751efb6a8fc9524
Philip Weimann <philip.weimann@aptiv.com>
2025-09-17 16:52:09 +0200

    refactor(autocommands): do not create a dir for oil buffers

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -56,6 +56,10 @@ vim.api.nvim_create_autocmd({ "CursorMoved", "ModeChanged" }, {
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
+        if vim.bo.filetype == "oil" or vim.api.nvim_buf_get_name(0) == "" then
+            return
+        end
+
         local dir = vim.fn.expand("<afile>:p:h")
         if vim.fn.isdirectory(dir) == 0 then
             vim.fn.mkdir(dir, "p")
