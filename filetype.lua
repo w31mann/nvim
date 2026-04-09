8dc83c66bdc30f861ea137aa3ff8d2c48137d17a
Philip Weimann <philip.weimann@aptiv.com>
2026-04-09 17:35:30 +0200

    feat(filetype): add nginx filetype detection for conf and inc files

--- filetype.lua
+++ filetype.lua
@@ -2,4 +2,8 @@ vim.filetype.add({
     extension = {
         Jenkinsfile = "groovy",
     },
+    pattern = {
+        [".*nginx.*%.conf"] = "nginx",
+        [".*nginx.*%.inc"] = "nginx",
+    },
 })
